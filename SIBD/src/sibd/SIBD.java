86328920d7269d1acc2f4846925309c9c2eb35a8
Triagushp <124849560+Triagushp@users.noreply.github.com>
2023-02-16 19:21:04 +0700

    Create SIBD.java

---
+++ SIBD/src/sibd/SIBD.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sibd;
+
+/**
+ *
+ * @author Tri agus hadi p
+ */
+public class SIBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    /*String nama = "Indonesia";
+    char abjad = 'a';
+    int a =6;
+    double b =6.9;
+    
+    System.out.println(nama);
+    System.out.println(b);*/
+    String nama = "Tri agus hadi prasetyo";
+    String nim = "E41220105";
+    String status = "Mahasiswa";
+    
+        System.out.println("nama ="+ nama);
+        System.out.println("nim =" + nim);
+        System.out.println("status =" + status);
+    }
+    
+}
